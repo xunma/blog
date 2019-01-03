7bf1c5f7014d484324f073e3a00f11b270bbd0a9
Xun Ma <lazybone1995@hotmail.com>
2019-01-03 21:25:53 +0800

    controllers set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true, allow_blank: false
   has_many :posts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'date'
+puts 'Destroying existing data...'
+Post.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+xun = User.new(first_name: 'Xunie', last_name: 'Ma', email: 'xun@mail.com', password: '123456')
+xun.save!
+
+3.times do
+  first_name = Faker::GreekPhilosophers.name
+  last_name = Faker::Cat.name
+  user = User.new(first_name: first_name, last_name: last_name, email: "#{first_name}@mail.com", password: '123456')
+  user.save!
+end
+
+puts 'creating posts...'
+(1..10).to_a.each do |num|
+  title = Faker::HitchhikersGuideToTheGalaxy.specie
+  content = Faker::HitchhikersGuideToTheGalaxy.quote
+  post = Post.new(
+    title: title,
+    content: content,
+    date: (Date.today - num),
+    user_id: rand(User.first.id..User.last.id)
+  )
+  post.save!
+end
+
+puts "You're all set!"
