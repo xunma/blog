cd21ee3c6f177d2d321ad0dca0e08b77587cba92
Xun Ma <lazybone1995@hotmail.com>
2019-01-04 20:59:25 +0800

    views basic content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    if params[:query].present?
+      sql_query = " \
+        posts.title @@ :query \
+        OR posts.content @@ :query \
+        OR users.first_name @@ :query \
+        OR users.last_name @@ :query \
+      "
+      @posts = Post.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @posts = Post.all
+    end
+
   end
 
   def show
@@ -33,6 +44,11 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
+  end
+
+  def my_posts
+    @user = User.find(params[:id])
+    @my_posts = Post.where(user: @user)
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   validates :content, presence: true, allow_blank: false
   validates :date, presence: true
   belongs_to :user
+
+  def read_time
+    word_count = self.content.split.size
+    "#{(word_count / 200.0).ceil} min read"
+  end
 end
